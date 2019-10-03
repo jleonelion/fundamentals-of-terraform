40546d133646646282d08abc797f6d43928c30e6
jleonelion <jleonelion@gmail.com>
2019-10-02 21:00:44 -0700

    misc changes while filming

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -12,6 +12,6 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = local.vnet_name
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
